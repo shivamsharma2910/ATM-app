9aa4babfcdfa32031f502c8a4ed3e7810a51edba
Shivam Sharma <Shivam.sharma101@t-mobile.com>
2021-07-07 20:06:35 +0530

    adding the basic ATM feature

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,72 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, within } from '@testing-library/react';
 import App from './App';
+import UserEvent from '@testing-library/user-event';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('ATM app', () => {
+  let checkboxes: Array<HTMLInputElement>;
+  let crunchButton: HTMLButtonElement;
+  let amountField: HTMLAllCollection;
+  let table: HTMLTableElement;
+
+  beforeEach(() => {
+    render(<App />);
+    checkboxes = screen.getAllByRole('checkbox');
+    crunchButton = screen.getByRole('button');
+    amountField = screen.queryByPlaceholderText('Amount in multiples of 10');
+    table = screen.getByRole("table");
+  });
+
+  test('verify that app is rendering correctly', () => {
+    expect(checkboxes.length).toBeGreaterThan(0);
+    expect(crunchButton).toBeInTheDocument();
+    expect(amountField).toBeInTheDocument();
+  });
+
+  test('validation is working for amount', () => {
+    UserEvent.type(amountField, '15');
+    const errorMessage = screen.getByText('Kindly enter positive amount in multiples of 10.');
+    expect(crunchButton).toBeDisabled();
+    expect(errorMessage).toBeInTheDocument();
+
+    UserEvent.type(amountField, '150');
+    expect(crunchButton).not.toBeDisabled();
+    expect(errorMessage).not.toBeInTheDocument();
+  });
+
+  test('notes are dispensed correctly with no denomination selection', () => {
+    UserEvent.type(amountField, '900');
+    UserEvent.click(crunchButton);
+    const [columnNames, ...rows] = within(table).getAllByRole("rowgroup");
+    expect(within(rows[0]).getByText(500)).toBeInTheDocument();
+    expect(within(rows[0]).getByText(1)).toBeInTheDocument();
+    expect(within(rows[0]).getByText(200)).toBeInTheDocument();
+    expect(within(rows[0]).getByText(2)).toBeInTheDocument();
+
+  });
+
+  test('notes are dispensed correctly with no denomination selection', () => {
+    UserEvent.click(checkboxes[2]);
+    UserEvent.type(amountField, '900');
+    UserEvent.click(crunchButton);
+    const [columnNames, ...rows] = within(table).getAllByRole("rowgroup");
+    expect(within(rows[0]).getByText(200)).toBeInTheDocument();
+    expect(within(rows[0]).getByText(4)).toBeInTheDocument();
+    expect(within(rows[0]).getByText(100)).toBeInTheDocument();
+    expect(within(rows[0]).getByText(1)).toBeInTheDocument();
+
+  });
+
+  test('notes are dispensed correctly with no denomination selection', () => {
+    UserEvent.click(checkboxes[2]);
+    UserEvent.click(checkboxes[4]);
+    UserEvent.type(amountField, '900');
+    UserEvent.click(crunchButton);
+    const [columnNames, ...rows] = within(table).getAllByRole("rowgroup");
+    expect(within(rows[0]).getByText(200)).toBeInTheDocument();
+    expect(within(rows[0]).getByText(4)).toBeInTheDocument();
+    expect(within(rows[0]).getByText(50)).toBeInTheDocument();
+    expect(within(rows[0]).getByText(2)).toBeInTheDocument();
+
+  });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,114 @@
 import React from 'react';
-import logo from './logo.svg';
+import { useState } from "react";
 import './App.css';
 
 function App() {
+  const denominations = [1000, 500, 200, 100, 50, 20, 10];
+  const [amount, setAmount] = useState(0);
+  const [validAmount, setValidAmount] = useState(true);
+  const [selectedIndex, setSelectedIndex] = useState(new Array(denominations.length).fill(false));
+  const [resultArray, setResultArray] = useState(new Array(denominations.length).fill(0))
+  const regexPattern = new RegExp('^[1-9][0-9]*0$');
+
+  // Validate entered amount and update state variables accordingly
+  function validateNumber(amount: string) {
+    if (amount === '') {
+      setValidAmount(true);
+    } else if(regexPattern.test(amount)) {
+      setAmount(parseInt(amount));
+      setValidAmount(true); 
+    } else {
+      setValidAmount(false);
+    }
+    setResultArray(new Array(denominations.length).fill(0))
+  }
+
+  //Figure out the different denominations count
+  function getNotes() {
+    let resultArray = [];
+    let tempAmount = amount;
+    // If denominations are selected
+    if (selectedIndex.includes(true)) {
+      for (let i = 0; i < denominations.length; i++) {
+        if (selectedIndex[i]) {
+          resultArray.push(Math.floor(tempAmount / denominations[i]));
+          tempAmount = tempAmount % denominations[i];
+        } else {
+          resultArray.push(0);
+        }
+      }
+      // If selected denominations can not make up to total amount
+      if (tempAmount !== 0) {
+        for (let i = 0; i < denominations.length; i++) {
+          if (resultArray[i] === 0) {
+            resultArray[i] = Math.floor(tempAmount / denominations[i])
+            tempAmount = tempAmount % denominations[i];
+          }
+        }
+      }
+      
+    } else {
+      //If no denominations are selected
+      for (let i = 0; i < denominations.length; i++) {
+        resultArray.push(Math.floor(tempAmount / denominations[i]));
+        tempAmount = tempAmount % denominations[i];
+      }
+    }
+    setResultArray(resultArray);
+  }
+
+  // Update the checkbox selection state
+  function handleOnChange(position: number) {
+    const updatedCheckedState = selectedIndex.map((item: boolean, index: number) =>
+      index === position ? !item : item
+    );
+    setSelectedIndex(updatedCheckedState);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>ATM APP </h1>
+      <label>Select Denominations if required</label>
+      <div>
+        {denominations.map((value, index) => {
+          return (
+              <div key={index} className="denominations-list-item">
+                <div className="left-section">
+                  <input
+                    type="checkbox"
+                    id={`custom-checkbox-${index}`}
+                    value={value}
+                    onChange={() => handleOnChange(index)}
+                  />
+                  <label htmlFor={`custom-checkbox-${index}`}>{value}</label>
+                </div>
+              </div>
+          );
+        })}
+        </div>
+        <div className="amount-section">
+          <span>Enter Amount</span>
+          <input step="10" className="amount-text" type="number" placeholder="Amount in multiples of 10" onChange={(e)=>validateNumber(e.target.value)}/>
+          <div>
+            <button className="crunch-button" disabled={!validAmount} onClick={e=>getNotes()}>Crunch</button>
+          </div>
+          { !validAmount && <div className="error-message">Kindly enter positive amount in multiples of 10.   </div> }
+
+            <table>
+              <thead>
+                <tr>
+                  <th>Denominations</th>
+                  <th>Number of notes</th>
+                </tr>
+              </thead>
+              <tbody>{denominations.map((value, index)=> {
+                return (
+                  (resultArray[index]!==0) && <tr key={index}><td>{value}</td><td>{resultArray[index]}</td></tr>
+                );
+              })}
+              </tbody>
+            </table>
+        </div>
     </div>
   );
 }
